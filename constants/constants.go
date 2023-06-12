dcd1abd667e789fd60a80291ba5de7b437009e8c
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-06-12 15:10:23 +0300

    constants file added

---
+++ constants/constants.go
@@ -0,0 +1,18 @@
+package constants
+
+const (
+	QUEUE = "queue-1"
+
+	USERNAME = "guest"
+	PASSWORD = "guest"
+	HOST     = "localhost"
+	PORT     = "5672"
+
+	TASK1 = "GetAllProducts"
+	TASK2 = "GetProductById"
+	TASK3 = "CreateProduct"
+	TASK4 = "EditProduct"
+	TASK5 = "DeleteProduct"
+
+	SERVERPORT = ":8000"
+)
